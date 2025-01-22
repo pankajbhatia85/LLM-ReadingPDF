5f8650f5e195350e9193fca221364f32c2f43c6e
Pankaj Bhatia <85344841+pankajbhatia85@users.noreply.github.com>
2025-01-21 23:44:23 -0500

    summarize_pdf

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from langchain.chains import QAWithSourcesChain,RetrievalQA
 from langchain.docstore.document import Document
 from pydantic import BaseModel
 from fastapi import FastAPI, File, UploadFile
-from PDF_Read1 import create_index1,uploaded_docs,load_model,new_model
+from tqdm import tqdm
+from openai import OpenAI
+from PDF_Read1 import create_index1,uploaded_docs,load_model,new_model,prepare_data
+from summary_functions import prepare_data_summarize,get_page_summary
 import pandas as pd
 import numpy as np
 import time
@@ -14,7 +17,36 @@ import os
 import openai
 import faiss
 import json
+from dotenv import load_dotenv
+load_dotenv()
+os.environ.get("OPENAI_API_KEY")
+OPEN_API_KEY=os.environ.get("OPENAI_API_KEY")
 
+app=FastAPI()
+
+client=OpenAI(api_key=OPEN_API_KEY)
+sys_prompt= """You are a research analyst and you are expert in reading the text of the document and summarize it in to 300 words."""
+user_prompt="""You are provided with the PDF document your task is to summarize the provided document in 300 words"""
+
+
+@app.post('/pdf_summarizer')
+async def pdf_summary(file:UploadFile):
+    page_summaries={}
+    file_location=f"./{file.filename}"
+    with open(file_location, "wb") as file_object:
+         file_object.write(await file.read())
+    My_pdf, len_pdf=uploaded_docs(file_location)
+    docs=prepare_data_summarize(My_pdf)
+    data_dict={}
+    for document in docs:
+        data_dict[document.metadata['source']]=document.page_content
+## Getting page wise summary
+    for page,content in data_dict.items():
+        page_summary=get_page_summary(content)
+        page_summaries[page]=page_summary
+    final_summary=get_page_summary(page_summaries)    
+    
+    
 
 app=FastAPI()
 
@@ -54,7 +86,7 @@ async def upload_file(file:UploadFile):
 
 # This Endpoint is used for querying the uploaded json
 @app.post("/query_json")
-async def query_file(item:PDF_File):
+async def query_json(item:PDF_File):
     with open ("./data.json",'r') as f:
         data = json.load(f)
 
---
+++ summary_functions.py
@@ -0,0 +1,71 @@
+from PyPDF2 import PdfReader
+from langchain.vectorstores import FAISS,faiss
+from langchain.embeddings import SentenceTransformerEmbeddings,HuggingFaceInstructEmbeddings,HuggingFaceHubEmbeddings,OpenAIEmbeddings,HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
+from langchain.chains import QAWithSourcesChain,RetrievalQA
+from langchain.docstore.document import Document
+from langchain.chat_models import ChatOpenAI
+from langchain import embeddings
+from tqdm import tqdm
+from openai import OpenAI
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+import time
+import os
+import openai
+import faiss
+import json
+from constants import *
+from dotenv import load_dotenv
+load_dotenv()
+os.environ.get("OPENAI_API_KEY")
+openai.api_key=OPEN_API_KEY
+
+client=OpenAI(api_key=OPEN_API_KEY)
+
+# We are using sentence transformer embeddings from HuggingFace
+embedding = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2", model_kwargs = {'device':'cpu'})
+
+# The function to read the pdf page to page and splitting the excerpts to chunks to create documents 
+def prepare_data_summarize(data):
+    combined_data = defaultdict(str)
+    for text, value in tqdm(data):
+        combined_data[value] += text + "\n" 
+#    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100,length_function=len)
+    processed_data = []
+    for source, combined_text in combined_data.items(): # type: ignore
+#        splits = text_splitter.split_text(combined_text)
+        processed_data.extend(
+            [
+                Document(
+                    page_content=combined_text,
+                    metadata={"source": source}
+                ) 
+            ]
+        )
+    
+    return processed_data
+
+def get_page_summary(content):
+    sys_prompt= """You are a research analyst and you are expert in reading the text of the document and summarize it in to 300 words."""
+    
+    if isinstance(content,str):
+        user_prompt=f"""You are provided with the PDF text:
+     content: {content} 
+     Your task is to summarize the provided document in 300 words"""
+    else:
+        user_prompt=f"""You are provided with the dictionary with key as the page numbers and values are the summaries of the respective page:
+     content: {content} 
+     Your task is to combine all the summaries and provide a collated final summary of the document in 300 words"""
+    response=client.chat.completions.create(model="gpt-4o",messages=[
+        {
+        "role":"system", "content":sys_prompt
+        },
+        {
+        "role":"user",
+        "content":user_prompt
+    }
+    ]
+    )
+    return response.choices[0].message.content
